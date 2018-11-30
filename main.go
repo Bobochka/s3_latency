05cb0ae835a3dade188b86cc2378bae78f9a1c96
Vladimir Shteinman <vladimir.s@cloudinary.com>
2018-11-30 22:15:02 +0200

    More details

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math"
 	"net"
 	"net/http"
+	"net/http/httptrace"
 	"sort"
 	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -59,6 +63,7 @@ func main() {
 	bucket := flag.String("bucket", "", "s3 bucket")
 	key := flag.String("key", "", "s3 key")
 	size := flag.Int("size", 30, "sample size")
+	concurrency := flag.Int("concurrency", 10, "concurrency")
 	ssl := flag.Bool("ssl", false, "whether enable ssl")
 
 	flag.Parse()
@@ -77,50 +82,97 @@ func main() {
 	}
 
 	wg := sync.WaitGroup{}
-	wg.Add(10)
+	wg.Add(*concurrency)
 
-	for j := 0; j < 10; j++ {
-		go func() {
+	for j := 0; j < *concurrency; j++ {
+		go func(threadID int) {
 			i := 0
+			firstByteSamples := []float64{}
+			beforeBodySamples := []float64{}
 			samples := []float64{}
+
+			var tt time.Time
+
+			trace := &httptrace.ClientTrace{
+				GotConn: func(_ httptrace.GotConnInfo) { tt = time.Now() },
+				GotFirstResponseByte: func() {
+					dtt := float64(time.Since(tt) / time.Millisecond)
+					firstByteSamples = append(firstByteSamples, dtt)
+				},
+			}
+
+			var reqID, reqID2, maxReqID, maxReqID2 string
+			var maxDur time.Duration
 
 			for {
 				i++
 				t := time.Now()
 
-				obj, err := svc.GetObject(input)
+				req, output := svc.GetObjectRequest(input)
+				if err != nil {
+					log.Println(err)
+				}
+
+				req.HTTPRequest = req.HTTPRequest.WithContext(httptrace.WithClientTrace(context.Background(), trace))
+
+				req.ApplyOptions(func(r *request.Request) {
+					start := time.Now()
+					r.Handlers.Complete.PushBack(func(req *request.Request) {
+						dtbb := float64(time.Since(start) / time.Millisecond)
+						beforeBodySamples = append(beforeBodySamples, dtbb)
+
+						reqID = req.RequestID
+						reqID2 = req.HTTPResponse.Header.Get("x-amz-id-2")
+					})
+				})
+
+				err := req.Send()
+
 				if err != nil {
 					log.Println(err.Error())
 				}
 
-				if obj != nil && obj.Body != nil {
-					ioutil.ReadAll(obj.Body)
-					obj.Body.Close()
+				if output != nil && output.Body != nil {
+					io.Copy(ioutil.Discard, output.Body)
+					output.Body.Close()
 				}
 
-				d := time.Since(t) / time.Millisecond
-				samples = append(samples, float64(d))
+				d := time.Since(t)
+				samples = append(samples, float64(d/time.Millisecond))
+
+				if d > maxDur {
+					maxReqID = reqID
+					maxReqID2 = reqID2
+					maxDur = d
+				}
 
 				if i%*size == 0 {
-					str := fmt.Sprintf("Sampled %v: ", len(samples))
-
-					sort.Float64s(samples)
-
-					str += fmt.Sprintf("min: %.3fms; ", samples[0])
-					str += fmt.Sprintf("max: %.3fms; ", samples[len(samples)-1])
-					str += fmt.Sprintf("50p: %.3fms; ", percentile(samples, 0.5))
-					str += fmt.Sprintf("75p: %.3fms; ", percentile(samples, 0.75))
-					str += fmt.Sprintf("95p: %.3fms; ", percentile(samples, 0.95))
+					logResults(threadID, firstByteSamples, beforeBodySamples, samples, maxReqID, maxReqID2)
 
 					samples = []float64{}
+					firstByteSamples = []float64{}
+					beforeBodySamples = []float64{}
+					maxDur = 0
 					i = 0
-
-					log.Println(str)
 				}
-
 			}
-		}()
+		}(j)
 	}
 
 	wg.Wait()
+}
+
+func logResults(threadID int, firstByteSamples, beforeBodySamples, samples []float64, reqID, reqID2 string) {
+	sort.Float64s(firstByteSamples)
+	sort.Float64s(beforeBodySamples)
+	sort.Float64s(samples)
+
+	str := fmt.Sprintf("---------------------- thread %v sampled %v\n", threadID, len(samples))
+	str += fmt.Sprintf("min: first byte: %.3fms; up to body: %.3fms; with body: %.3fms;\n", firstByteSamples[0], beforeBodySamples[0], samples[0])
+	str += fmt.Sprintf("max: first byte: %.3fms; up to body: %.3fms; with body: %.3fms;\n", firstByteSamples[len(firstByteSamples)-1], beforeBodySamples[len(beforeBodySamples)-1], samples[len(samples)-1])
+	str += fmt.Sprintf("50p: first byte: %.3fms; up to body: %.3fms; with body: %.3fms;\n", percentile(firstByteSamples, 0.5), percentile(beforeBodySamples, 0.5), percentile(samples, 0.5))
+	str += fmt.Sprintf("75p: first byte: %.3fms; up to body: %.3fms; with body: %.3fms;\n", percentile(firstByteSamples, 0.75), percentile(beforeBodySamples, 0.75), percentile(samples, 0.75))
+	str += fmt.Sprintf("95p: first byte: %.3fms; up to body: %.3fms; with body: %.3fms;\n", percentile(firstByteSamples, 0.95), percentile(beforeBodySamples, 0.95), percentile(samples, 0.95))
+	str += fmt.Sprintf("Slowest request: req-id: %s x-amz-id-2: %s\n", reqID, reqID2)
+	log.Println(str)
 }
